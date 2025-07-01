b4b6903b0ba825ae5d704df2ee43210388a6f580
ferecovasevinc <sevafrc@gmail.com>
2025-07-01 22:21:33 +0400

    Added Entities

---
+++ src/Core/Mini E-Ticarət API.Domain/Entities/AppUser.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Mini_E_Ticarət_API.Domain.Entities;
+
+public class AppUser : IdentityUser
+{
+}
---
+++ src/Core/Mini E-Ticarət API.Domain/Entities/Category.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_E_Ticarət_API.Domain.Entities
+{
+    internal class Category
+    {
+    }
+}
