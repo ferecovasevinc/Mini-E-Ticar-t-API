7fe46a044b7292b47fa778c88891db4b5d7fc19e
ferecovasevinc <sevafrc@gmail.com>
2025-07-07 20:38:10 +0400

    added image

---
+++ src/Core/Mini E-Ticarət API.Application/Validations/ImageValidators/ImageCreateDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using Mini_E_Ticarət_API.Application.DTOs.ImageDtos;
+
+namespace Mini_E_Ticarət_API.Application.Validations.ImageValidators;
+
+public class ImageCreateDtoValidator : AbstractValidator<ImageCreateDto>
+{
+    public ImageCreateDtoValidator()
+    {
+        RuleFor(i => i.ImageUrl)
+            .NotEmpty().WithMessage("Image URL must not be empty.")
+            .MaximumLength(500).WithMessage("Image URL is too long. Maximum 500 characters allowed.");
+
+        RuleFor(i => i.ProductId)
+            .NotEmpty().WithMessage("Product ID is required.");
+    }
+}
---
+++ src/Core/Mini E-Ticarət API.Application/Validations/ImageValidators/ImageUpdateDtoValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Mini_E_Ticarət_API.Application.DTOs.ImageDtos;
+
+namespace Mini_E_Ticarət_API.Application.Validations.ImageValidators;
+
+public class ImageUpdateDtoValidator : AbstractValidator<ImageUpdateDto>
+{
+    public ImageUpdateDtoValidator()
+    {
+        RuleFor(i => i.ImageUrl)
+            .NotEmpty().WithMessage("Image URL must not be empty.")
+            .MaximumLength(500).WithMessage("Image URL is too long. Maximum 500 characters allowed.");
+    }
+}
