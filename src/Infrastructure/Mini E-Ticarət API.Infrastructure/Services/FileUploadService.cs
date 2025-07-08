5a38140a13ecdf8cb163f5f9ced91805d48a6a9d
ferecovasevinc <sevafrc@gmail.com>
2025-07-09 01:26:03 +0400

    FileUploadService

---
+++ src/Infrastructure/Mini E-Ticarət API.Infrastructure/Services/FileUploadService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Mini_E_Ticarət_API.Application.Abstracts.Services;
+
+namespace Mini_E_Ticarət_API.Infrastructure.Services;
+
+public class FileUploadService : IFileUploadService
+{
+    private  IWebHostEnvironment _env { get; }
+    public FileUploadService(IWebHostEnvironment env)
+    {
+        _env = env;
+    }
+
+    public async Task<string> UploadAsync(IFormFile file)
+    {
+        var uploadsFolder = Path.Combine(_env.WebRootPath, "uploads");
+        if (!Directory.Exists(uploadsFolder))
+            Directory.CreateDirectory(uploadsFolder);
+
+        var fileName = GetUniqueFileName(uploadsFolder, file.FileName);
+        var filePath = Path.Combine(uploadsFolder, fileName);
+
+        using var stream = new FileStream(filePath, FileMode.Create);
+        await file.CopyToAsync(stream);
+
+        return $"/uploads/{fileName}";
+    }
+
+
+    private string GetUniqueFileName(string uploadsFolderPath, string originalFileName)
+    {
+        var fileNameWithoutExtension = Path.GetFileNameWithoutExtension(originalFileName);
+        var fileExtension = Path.GetExtension(originalFileName);
+        var fileName = fileNameWithoutExtension + fileExtension;
+        var counter = 1;
+
+        while (File.Exists(Path.Combine(uploadsFolderPath, fileName)))
+        {
+            fileName = $"{fileNameWithoutExtension}({counter}){fileExtension}";
+            counter++;
+        }
+
+        return fileName;
+    }
+}
