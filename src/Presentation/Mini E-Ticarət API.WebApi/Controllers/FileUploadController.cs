bd94033f5a1145e455caf4709f320a2a394c8bd9
ferecovasevinc <sevafrc@gmail.com>
2025-07-09 01:39:12 +0400

    Created FileUpload

---
+++ src/Presentation/Mini E-Ticarət API.WebApi/Controllers/FileUploadController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mini_E_Ticarət_API.Application.Abstracts.Services;
+using Mini_E_Ticarət_API.Application.DTOs.FileUploadDtos;
+
+namespace Mini_E_Ticarət_API.WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class FileUploadController : ControllerBase
+{
+    private IFileUploadService _fileUploadService {  get; }
+    public FileUploadController(IFileUploadService fileUploadService)
+    {
+        _fileUploadService = fileUploadService;
+    }
+
+
+    [HttpPost("upload")]
+    public async Task<IActionResult> Upload([FromForm] FileUploadDto dto)
+    {
+        var fileUrl = await _fileUploadService.UploadAsync(dto.File);
+        return Ok(new { FileUrl = fileUrl });
+    }
+}
